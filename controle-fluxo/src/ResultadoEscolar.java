9187b963a2244b35678115c47ffc4f4696aa0587
ricardopanighel <ricardopanighellazarini@gmail.com>
2024-09-04 21:19:18 -0300

    Condição Ternária

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -2,13 +2,9 @@ public class ResultadoEscolar {
     public static void main(String[] args) {
         int nota = 6;
 
-        if (nota >= 7)
-            System.out.println("Aprovado");
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
 
-        else if (nota>=5 && nota<7)
-            System.out.println("Prova de Recuperação");
+        System.out.println(resultado);
 
-        else
-            System.out.println("Reprovado");
     }
 }
